872124c67f2a8866037146ce3f4a7b0e777c6097
ShahinHossaain <mdshahinhossaain@gmail.com>
2023-05-21 23:22:29 +0600

    comment added'

--- src/Pages/Home/Comment/Comment.jsx
+++ src/Pages/Home/Comment/Comment.jsx
@@ -3,9 +3,11 @@ const Comment = () => {
     <div>
       <div className="mx-3 md:mx-20 card shadow-lg p-3 md:p-10 bg-base-200 mt-24">
         <p className="text-3xl md:text-5xl text-center o_font3 tracking-2 font-bold font-serif ">
-          <span className="text-green-500">SocialChef</span> in numbers
+          <span className="text-center text-5xl font-bold font-font3 text-transparent bg-clip-text bg-gradient-to-r from-gray-400 to-secondary mb-12">
+            WebSite Details{" "}
+          </span>
         </p>
-        <div className="grid grid-cols-3 md:grid-cols-6 gap-3 justify-between mt-7">
+        <div className="grid grid-cols-3 md:grid-cols-6 gap-3 justify-between mt-7 text-black">
           <div className="bg-slate-300 px-4 md:px-8 w-full py-2 md:py-4 text-center transition duration-200 ease-in-out hover:bg-gray-500">
             <p className="text-3xl font-extrabold transition duration-200 ease-in-out hover:text-gray-100">
               3<span className="">0</span>
