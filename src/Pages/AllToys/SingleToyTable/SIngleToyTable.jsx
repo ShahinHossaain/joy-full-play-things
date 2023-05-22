4fec10cf0dee598a7e601c8d735b1df95e62d483
ShahinHossaain <mdshahinhossaain@gmail.com>
2023-05-23 01:48:04 +0600

    done

--- src/Pages/AllToys/SingleToyTable/SIngleToyTable.jsx
+++ src/Pages/AllToys/SingleToyTable/SIngleToyTable.jsx
@@ -1,12 +1,38 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { AwesomeButton } from "react-awesome-button";
 import AllToysDetails from "../../AllToysDetails/AllToysDetails";
+import { AuthContext } from "../../../Provider/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const SIngleToyTable = ({ toy }) => {
-  const { sellerName, toyName, subCategory, price, availableQuantity } = toy;
+  const { sellerName, toyName, subCategory, price, availableQuantity, _id } =
+    toy;
   const [open, setOpen] = useState(false);
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
   const handleDetails = () => {
-    setOpen(true);
+    console.log("click");
+
+    if (user) setOpen(true);
+    // else navigate("/login");9*6*
+    else {
+      Swal.fire({
+        title: "Hey!!!",
+        text: "You have to log in first to view details",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Go to Login???",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const pathname = `/toyDetails/${_id}`;
+          Swal.fire("", "We are going Login page.", "success");
+          navigate("/login", { state: { pathname } });
+        }
+      });
+    }
     console.log("click");
   };
   return (
